bdc8aa14c91211d88685a33e8a0436d5c9074638
VincentReynaert <vincent.reynaert@isen.yncrea.fr>
2016-11-11 15:00:36 +0100

    maj du logger

--- src/logger.py
+++ src/logger.py
@@ -10,11 +10,35 @@
 import logging
 import logging.config
 import os
+import os.path
 
 
 # ____________________________________________________________________________________________________
 # ____________________________________________________________________________________________________
 # Fonctions d'initialisation
+def creeLog(logPath):
+    """
+
+    :param logPath:
+    :return: MAIN_LOGGER
+    :rtype: log
+    """
+    if os.path.isdir(logPath):
+        nameFile = os.path.join(logPath, "rsyncFTP.log")
+    elif os.path.isfile(logPath):
+        nameFile = logPath
+    else :
+        nameFile = "rsyncFTP.log"
+    logging.basicConfig( \
+        filename=nameFile, \
+        datefmt="%d/%m/%Y-%H:%M:%S", \
+        format="%(asctime)s %(levelname)s %(funcName)s %(message)s", \
+        level=logging.INFO)  # 'filename': '/path/to/rsyncFTP.debug.log',
+    MAIN_LOGGER = logging.getLogger("rsyncFTP")
+    MAIN_LOGGER.info("Programme lance")
+    return MAIN_LOGGER
+
+
 def initLog(logPath, logConf):
     """
     log format
@@ -24,15 +48,19 @@ def initLog(logPath, logConf):
     :return: MAIN_LOGGER
     :rtype: log
     """
-    logging.config.fileConfig("rsyncFTP.conf")
-    # logging.basicConfig(filename=os.path.join(logPath, "pioupiou.log"))
+    if logPath == "":
 
-    # definition du handler
-    MAIN_LOGGER = logging.getLogger("rsyncLocal")
-    log2 = logging.getLogger("rsyncFile")
+        logging.config.fileConfig(logConf)
+        # logging.basicConfig(filename=os.path.join(logPath, "pioupiou.log"))
 
-    MAIN_LOGGER.info("Programme lance")
-    log2.info("Programme lance test file")
+        # definition du handler
+        MAIN_LOGGER = logging.getLogger("rsyncLocal")
+        log2 = logging.getLogger("rsyncFile")
+
+        MAIN_LOGGER.info("Programme lance")
+        log2.info("Programme lance test file")
+    else :
+        MAIN_LOGGER = creeLog(logPath)
 
     return MAIN_LOGGER
 
@@ -41,8 +69,7 @@ def initLog(logPath, logConf):
 # ____________________________________________________________________________________________________
 # Test unitaire
 def monMain():
-    MAIN_LOGGER = initLog("/media/nicolas/DE581D27581CFFC7/Users/Nicolas/Documents/Ecole/ISEN/Python",
-                          "/media/nicolas/DE581D27581CFFC7/Users/Nicolas/Documents/Ecole/ISEN/Python")
+    MAIN_LOGGER = initLog("C:\\Users\\vvinc_000\\Desktop","")
 
 
 if __name__ == "__main__":
