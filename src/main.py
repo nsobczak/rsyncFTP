c8e46c6bf35cef96895988b6c89d56b57e5515bd
nsobczak <nicolas.sobczak@isen.yncrea.fr>
2016-11-18 21:48:46 +0100

    Ajout fonctionalité gestion des fichiers à inclures/exclures

--- src/main.py
+++ src/main.py
@@ -58,12 +58,11 @@ def initialisationDossierFTP(args, logger, connectFTP):
     :type connectFTP: class 'ftplib.FTP'
     """
     dossier_chemin_absolu, dossier_nom = os.path.split(args.dp)
-    dossier_chemin_relatif = dossier_nom
     suppression = gestionFTP.supprimerDossier(ftp=connectFTP, dossier_chemin="", dossier_nom=dossier_nom)
-    if not suppression :
+    if not suppression:
         logger.debug('Le dossier est bien supprime.')
-    else :
-        logger.debug("Le dossier n'a pas pu etre supprime car il n'existe pas." )
+    else:
+        logger.debug("Le dossier n'a pas pu etre supprime car il n'existe pas.")
     gestionFTP.copierContenuDossier(ftp=connectFTP, chemin_ftp="", chemin_local=args.dp, nom_dossier=dossier_nom,
                                     profondeure_copie_autorisee=args.profondeur)
     logger.info("initialisation du dossier")
@@ -87,125 +86,198 @@ def donneCheminRelatif(args, chemin_element):
     return chemin_relatif
 
 
-def updateFTP_M(args, logger, connectFTP, M):
+def isFileToBeIncluded(includes, excludes, nom_fichier):
+    """
+    Fonction qui retourne vrai si le fichier a une extension que l'on souhaite copier
+    :param includes: liste des fichiers a inclure
+    :param excludes: liste des fichiers a exclure
+    :param nom_fichier: nom du fichier avec son extension
+    :type nom_fichier: str
+    :return: result
+    :rtype: bool
+    """
+    result = False
+    # on recupere l'extension du fichier
+    root, extension = os.path.splitext(nom_fichier)
+
+    # cas 1 : on inclut toutes les extensions sauf certaines
+    if ((includes[0] == "*") and (not extension in excludes)):
+        result = True
+    # cas 2 : on exclut toutes les extensions sauf certaines
+    else:
+        if ((excludes[0] == "*") and (extension in includes)):
+            result = True
+
+    return result
+
+
+def updateFTP_M(args, logger, connectFTP, includes, excludes, M):
     """
     Fonction qui s'occupe de mettre a jour le dossier situe sur le serveur FTP en cas d'ajout
     :param args: parametres entres en lignes de commandes
     :param logger: fichier de log
     :param connectFTP: objet ftp
+    :param includes: liste des fichiers a inclure
+    :param excludes: liste des fichiers a exclure
     :param M: liste des elements modifies
     :type args: dict
     :type logger: log
     :type connectFTP: class 'ftplib.FTP'
+    :type includes: list
+    :type excludes: list
     :type M: list
     """
     for tup in M:
         path = tup[0]
+
         # Si modification d'un fichier => remplacement
         if os.path.isfile(path):
             fichier_chemin_absolu, fichier_nom = os.path.split(path)
-            fichier_chemin_relatif = donneCheminRelatif(args.dp, path)
-            gestionFTP.supprimerFichier(ftp=connectFTP, fichier_chemin=fichier_chemin_relatif, fichier_nom=fichier_nom)
-            gestionFTP.envoyerUnFichier(ftp=connectFTP, fichier_chemin=path, fichier_nom=fichier_nom)
+            # Si l'extension du fichier est a inclure
+            if isFileToBeIncluded(includes, excludes, fichier_nom):
+                fichier_chemin_relatif = donneCheminRelatif(args.dp, path)
+                gestionFTP.supprimerFichier(ftp=connectFTP, fichier_chemin=fichier_chemin_relatif,
+                                            fichier_nom=fichier_nom)
+                gestionFTP.envoyerUnFichier(ftp=connectFTP, fichier_chemin=path, fichier_nom=fichier_nom)
+
         # Les modifications de dossiers ne sont pas prises en compte ici
-        # car elles sont assimilees a une suppression et creation
+        # car elles sont assimilees a une suppression suivie d'une creation
         else:
             logger.info(path + " n'est pas supporte par rsyncFTP")
 
 
-def updateFTP_A(args, logger, connectFTP, A):
+def updateFTP_A(args, logger, connectFTP, includes, excludes, A):
     """
     Fonction qui s'occupe de mettre a jour le dossier situe sur le serveur FTP en cas d'ajout
     :param args: parametres entres en lignes de commandes
     :param logger: fichier de log
     :param connectFTP: objet ftp
+    :param includes: liste des fichiers a inclure
+    :param excludes: liste des fichiers a exclure
     :param A: liste des elements ajoutes
     :type args: dict
     :type logger: log
     :type connectFTP: class 'ftplib.FTP'
+    :type includes: list
+    :type excludes: list
     :type A: list
     """
     for tup in A:
         path = tup[0]
+
         # Si ajout d'un fichier
         if os.path.isfile(path):
             fichier_chemin_absolu, fichier_nom = os.path.split(path)
-            gestionFTP.positionnementDansLeFTP(connectFTP, chemin_relatif)
-            gestionFTP.envoyerUnFichier(fichier_chemin=path, ftp=connectFTP, fichier_nom=fichier_nom)
+            # Si l'extension du fichier est a inclure
+            if isFileToBeIncluded(includes, excludes, fichier_nom):
+                fichier_chemin_relatif = donneCheminRelatif(args.dp, path)
+                gestionFTP.positionnementDansLeFTP(connectFTP, fichier_chemin_relatif)
+                gestionFTP.envoyerUnFichier(fichier_chemin=path, ftp=connectFTP, fichier_nom=fichier_nom)
+
         # Si ajout d'un dossier
         elif os.path.isdir(path):
             # chemin local et nom dossier a recuperer en separant le chemin absolu
             dossier_chemin_absolu, dossier_nom = os.path.split(path)
             gestionFTP.copierContenuDossier(ftp=connectFTP, chemin_ftp="", chemin_local=path, nom_dossier=dossier_nom,
                                             profondeure_copie_autorisee=args.profondeur)
+
         else:
             logger.info(path + " n'est pas supporte par rsyncFTP")
 
 
-def updateFTP_D(args, logger, connectFTP, D):
+def updateFTP_D(args, logger, connectFTP, includes, excludes, D):
     """
     Fonction qui s'occupe de mettre a jour le dossier situe sur le serveur FTP en cas d'ajout
     :param args: parametres entres en lignes de commandes
     :param logger: fichier de log
     :param connectFTP: objet ftp
+    :param includes: liste des fichiers a inclure
+    :param excludes: liste des fichiers a exclure
     :param D: liste des elements supprimes
     :type args: dict
     :type logger: log
     :type connectFTP: class 'ftplib.FTP'
+    :type includes: list
+    :type excludes: list
     :type D: list
     """
     for tup in D:
         path = tup[0]
+
         # Si suppression d'un fichier
         if os.path.isfile(path):
             fichier_chemin_absolu, fichier_nom = os.path.split(path)
-            fichier_chemin_relatif = donneCheminRelatif(args.dp, path)
-            gestionFTP.supprimerFichier(ftp=connectFTP, fichier_chemin=fichier_chemin_relatif, fichier_nom=fichier_nom)
+            # Si l'extension du fichier est a inclure
+            if isFileToBeIncluded(includes, excludes, fichier_nom):
+                fichier_chemin_relatif = donneCheminRelatif(args.dp, path)
+                gestionFTP.supprimerFichier(ftp=connectFTP, fichier_chemin=fichier_chemin_relatif,
+                                            fichier_nom=fichier_nom)
+
         # Si suppression d'un dossier
         elif os.path.isdir(path):
             dossier_chemin_absolu, dossier_nom = os.path.split(path)
             dossier_chemin_relatif = donneCheminRelatif(args.dp, path)
             gestionFTP.supprimerDossier(ftp=connectFTP, dossier_chemin=dossier_chemin_relatif, dossier_nom=dossier_nom)
+
         else:
             logger.info(path + " n'est pas supporte par rsyncFTP")
 
 
-def updateFTP(args, logger, connectFTP, M, A, D):
+def updateFTP(args, logger, connectFTP, includes, excludes, M, A, D):
     """
     Fonction qui gere la mise a jour du dossier distant
     :param args: parametres entres en lignes de commandes
-    :param logger:
+    :param logger: fichier de log
+    :param connectFTP: objet ftp
+    :param includes: liste des fichiers a inclure
+    :param excludes: liste des fichiers a exclure
+    :param M:
+    :param A:
+    :param D:
     :type args: dict
     :type logger: log
+    :type connectFTP: class 'ftplib.FTP'
+    :type includes: list
+    :type excludes: list
+    :type M: list
+    :type A: list
+    :type D: list
     :param connectFTP: objet ftp
     :type connectFTP: class 'ftplib.FTP'
     """
     # Si ajout
     if (A != []):
-        updateFTP_A(args, logger, connectFTP, A)
+        updateFTP_A(args, logger, connectFTP, includes, excludes, A)
     # Si suppression
     if (D != []):
-        updateFTP_D(args, logger, connectFTP, D)
+        updateFTP_D(args, logger, connectFTP, includes, excludes, D)
     # Si modification
     if (M != []):
-        updateFTP_M(args, logger, connectFTP, M)
+        updateFTP_M(args, logger, connectFTP, includes, excludes, M)
 
 
 # %%__________________________________________________________________________________________________
-def loop(args, logger, arbrePrecedent, startingLevel, connectFTP):
+def loop(args, logger, arbrePrecedent, startingLevel, connectFTP, includes, excludes):
     """
     Fonction de boucle principale. Elle fonctionne en 2 actions:
     - Surveiller
     - Si modif, mettre a jour le serveur FTP
     :param args: parametres entres en lignes de commandes
     :param logger: fichier de log
+    :param arbrePrecedent:
+    :param startingLevel: niveau de profondeur de depart
+    :param connectFTP: objet ftp
+    :param includes: liste des fichiers a inclure
+    :param excludes: liste des fichiers a exclure
     :type args: dict
     :type logger: log
-    :param connectFTP: objet ftp
+    :type arbrePrecedent:
+    :type startingLevel: int
     :type connectFTP: class 'ftplib.FTP'
-
-
+    :type includes: list
+    :type excludes: list
     :return: 1
+    :rtype: int
     """
     # Var
     frequence = args.frequence
@@ -238,7 +310,7 @@ def loop(args, logger, arbrePrecedent, startingLevel, connectFTP):
             if len(M) or len(A) or len(D):
                 arbrePrecedent = nouvelArbre
                 directorySupervisor.logTheMADLists(logger, M, A, D)
-                updateFTP(args, logger, connectFTP, M, A, D)
+                updateFTP(args, logger, connectFTP, includes, excludes, M, A, D)
             totalTime += 1
 
     return 1
@@ -247,6 +319,13 @@ def loop(args, logger, arbrePrecedent, startingLevel, connectFTP):
 # %%__________________________________________________________________________________________________
 # ____________________________________________________________________________________________________
 def monMain():
+    """Tests Unitaires
+    print(isFileToBeIncluded(['*'], ['.odt', ".docx"], "/home/nicolas/Documents/fichier.docx"))
+    print(isFileToBeIncluded(['*'], ['.odt', ".docx"], "/home/nicolas/Documents/fichier.txt"))
+    print(isFileToBeIncluded(['.odt', ".docx"], ['*'], "/home/nicolas/Documents/fichier.docx"))
+    print(isFileToBeIncluded(['.odt', ".docx"], ['*'], "/home/nicolas/Documents/fichier.txt"))
+    """
+
     # === Initialisation des variables ===
     ARGS = prftp.initVariables()
     buffer = init(ARGS)
@@ -262,7 +341,7 @@ def monMain():
     prftp.logArgs(ARGS, MAIN_LOGGER)
 
     # === Boucle principale ===
-    loop(ARGS, MAIN_LOGGER, arbrePrecedent, STARTINGLEVEL, connectFTP)
+    loop(ARGS, MAIN_LOGGER, arbrePrecedent, STARTINGLEVEL, connectFTP, INCLUDES, EXCLUDES)
 
 
 if __name__ == "__main__":
